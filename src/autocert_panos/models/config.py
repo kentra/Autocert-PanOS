34f487473884d775c35694f5d6811086f82ca089
Daniel Kolstad <admin@kentra.org>
2025-10-21 01:09:32 +0200

    Name change

---
+++ src/autocert_panos/models/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List, Optional
+from pydantic import SecretStr
+
+
+class Panos(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".secrets/panos/.panos.env"),
+        env_file_encoding="utf-8",
+    )
+    MGMT: str
+    API_KEY: SecretStr
+    TLS_PROFILE: str
+
+
+class CertBot(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".secrets/certbot/.domeneshop.env"),
+        env_file_encoding="utf-8",
+    )
+    DOMENESHOP_TOKEN: SecretStr
+    DOMENESHOP_SECRET: SecretStr
+    CERT_NAME: str
+    EMAIL: Optional[str] = None
+    DOMAIN_LIST: List[str] = []
